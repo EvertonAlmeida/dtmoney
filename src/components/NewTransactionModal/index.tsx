f3c26b72d5044e35ebdfaf8960b3136a79318c79
Everton Almeida <evertonalmeida@MacBook-Air.local>
2022-01-02 17:28:52 -0300

    add modal transaction style

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
-import Modal from "react-modal";
+import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,8 +12,25 @@ export const NewTransactionModal = ({ isOpen, onRequestClose }:NewTransactionMod
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+			overlayClassName="react-modal-overlay"
+			className="react-modal-content"
         >
-            <h2>Register Transaction</h2>
+			<button
+				type="button"
+				onClick={onRequestClose}
+				className="react-modal-close"
+			>
+				<img src={closeImg} alt="Close modal"></img>
+			</button>
+			<Container>
+            	<h2>Register Transaction</h2>
+
+				<input placeholder="Title"/>
+				<input type="number" placeholder="Value"/>
+				<input type="text" placeholder="Category" />
+
+				<button type="submit">Add</button>
+			</Container>
         </Modal>
     );
 };
