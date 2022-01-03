853b9f5f3628cbe17686a7d1dc8d32a54f680309
EvertonAlmeida <everton.o.almeida@outlook.com>
2022-01-03 18:43:31 -0300

    adding summary to the app

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,7 +7,22 @@ import { Container } from "./style"
 
 export const Summary = () => {
 	const { transactions } = useContext(TransactionsContext);
-	console.log(transactions);
+
+	const summary = transactions.reduce((acc, transaction) => {
+		if ( transaction.type === 'deposit') {
+			acc.incomings += transaction.amount;
+			acc.total += transaction.amount;
+		}else {
+			acc.outcomings += transaction.amount;
+			acc.total -= transaction.amount;
+		}
+
+		return acc;
+	}, {
+		incomings: 0,
+		outcomings: 0,
+		total: 0,
+	})
 
     return (
         <Container>
@@ -16,21 +31,31 @@ export const Summary = () => {
                     <p>Incomes</p>
                     <img src={incomeImg} alt="Incomes" />
                 </header>
-                <strong>$1000,00</strong>
+                <strong>{new Intl.NumberFormat('en-US', {
+						style: 'currency',
+						currency: 'USD'
+					}).format(summary.incomings)}</strong>
             </div>
             <div>
                 <header>
                     <p>Outcomes</p>
                     <img src={outcomeImg} alt="Outcomes" />
                 </header>
-                <strong>- $500,00</strong>
+                <strong>-{new Intl.NumberFormat('en-US', {
+						style: 'currency',
+						currency: 'USD'
+					}).format(summary.outcomings)}
+				</strong>
             </div>
             <div className='highlight-background'>
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>$500,00</strong>
+                <strong>{new Intl.NumberFormat('en-US', {
+						style: 'currency',
+						currency: 'USD'
+					}).format(summary.total)}</strong>
             </div>
         </Container>
     )
